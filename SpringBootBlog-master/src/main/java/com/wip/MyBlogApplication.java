ae4e116bb67074dacc63d986641705e2d1a10a7f
DESKTOP-KLI0Q7L\siqin <sqgrl220@gmail.com>
2023-09-05 16:48:31 +0800

    第一次

---
+++ SpringBootBlog-master/src/main/java/com/wip/MyBlogApplication.java
@@ -0,0 +1,18 @@
+package com.wip;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@MapperScan("com.wip.dao")
+@EnableSwagger2
+//@EnableCaching
+public class MyBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBlogApplication.class, args);
+    }
+}
